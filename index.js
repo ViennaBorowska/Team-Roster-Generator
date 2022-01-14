e019a83d95d2efa6780b1e49cdfe0dd93442f408
Vienna Borowska <vienna.borowska@hotmail.com>
2022-01-14 15:59:13 +0000

    inquirer questions written for all team members

--- index.js
+++ index.js
@@ -1,2 +1,175 @@
+// Packages
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+//Global variables
+const myTeam = [];
+
+//Questions for each role
+////Manager Qs
+const managerQs = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: `What is the team manager's name?`,
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: `What is the team manager's id?`,
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: `What is the team manager's email?`,
+      },
+      {
+        type: 'input',
+        name: 'officeNum',
+        message: `What is the team manager's office number?`,
+      },
+      {
+        type: 'list',
+        name: 'newMember',
+        message: `What type of team member would you like to add?`,
+        choices: [
+          'Engineer',
+          'Intern',
+          "I don't want to add any more team members",
+        ],
+      },
+    ])
+    .then((managerAns) => {
+      const manager = new Manager(
+        managerAns.id,
+        managerAns.name,
+        managerAns.email,
+        managerAns.officeNum
+      );
+      team.push(manager);
+      switch (managerAns.newMember) {
+        case 'Engineer':
+          engineerQuestions();
+          break;
+        case 'Intern':
+          internQuestions();
+          break;
+        default:
+          writeToFile('dist/index.html', generateTeam(myTeam));
+      }
+    });
+};
+
+const engineerQs = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the engineer's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the engineer's id?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's GitHub username?",
+      },
+      {
+        type: 'list',
+        name: 'newMember',
+        message: 'What type of team member would you like to add next?',
+        choices: [
+          'Engineer',
+          'Intern',
+          "I don't want to add any more team members",
+        ],
+      },
+    ])
+    .then((engineerAns) => {
+      const engineer = new Engineer(
+        engineerAns.id,
+        engineerAns.name,
+        engineerAns.email,
+        engineerAns.github
+      );
+      team.push(engineer);
+      switch (engineerAns.newMember) {
+        case 'Engineer':
+          engineerQuestions();
+          break;
+        case 'Intern':
+          internQuestions();
+          break;
+        default:
+          writeToFile('/index.html', generateTeam(myTeam));
+      }
+    });
+};
+
+const internQs = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the intern's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the intern's id?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the intern's email address?",
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: "What is the intern's school?",
+      },
+      {
+        type: 'list',
+        name: 'newMember',
+        message: 'What type of team member would you like to add next?',
+        choices: [
+          'Engineer',
+          'Intern',
+          "I don't want to add any more team members",
+        ],
+      },
+    ])
+    .then((internAns) => {
+      const intern = new Intern(
+        internAns.id,
+        internAns.name,
+        internAns.email,
+        internAns.school
+      );
+      team.push(intern);
+      switch (internAns.newMember) {
+        case 'Engineer':
+          engineerQuestions();
+          break;
+        case 'Intern':
+          internQuestions();
+          break;
+        default:
+          writeToFile('dist/index.html', generateTeam(myTeam));
+      }
+    });
+};
+
+managerQs();
